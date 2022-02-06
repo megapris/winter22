f045ccacb495b4ebf8250decac09015ac37ada53
Priscila Ontiveros <megapris@pop-os.localdomain>
2022-02-05 20:11:48 -0500

    low key works

--- cis343/project1/main.c
+++ cis343/project1/main.c
@@ -3,23 +3,31 @@
 
 int main(int argc, char** argv){
 	lite_vector * lv = lv_new_vec(sizeof(int*));
+	
+	
 	int x[1000];
-	for(int i=0; i<1000; ++i) {
-		x[i] = i;
-		printf("Array addres: %p\n", (x + i * sizeof(int*)));
+
+
+	for(int i=0; i<1; ++i) {
+		x[i] = 127;
+		printf("Main Array address: %p\n", (x + i * sizeof(int*)));
 		lv_append(lv, &(x[i]));
 	};
+	x[1]=200;
 
-	for(int i=0; i<1000; ++i){
-		int * p = lv_get(lv, i);
-		int val = *p;
-		printf("%i\t", val);
-	}
 
-	lite_vector * lv2 = lv_new_vec(sizeof(char*));
-	lv_append(lv2, "Mr. ");
-	lv_append(lv2, "W. ");
-	lv_append(lv2, "says ");
-	lv_append(lv2, "hi!");
-	printf("%s\n", (char*)lv_get(lv2, 0));
+	// for(int i=0; i<2; ++i){
+	// 	int * p = lv_get(lv, i);
+	// 	int val = *p;
+	// 	printf("%i\t", val);
+	// }
+
+	// lite_vector * lv2 = lv_new_vec(sizeof(char*));
+	// lv_append(lv2, "Mr. ");
+	// lv_append(lv2, "W. ");
+	// lv_append(lv2, "says ");
+	// lv_append(lv2, "hi!");
+	// printf("%s\n", (char*)lv_get(lv2, 0));
 }
+
+
--- cis343/project1/student_lite_vector.c
+++ cis343/project1/student_lite_vector.c
@@ -4,18 +4,46 @@
 #include <string.h>
 
 lite_vector* lv_new_vec(size_t type_size){
+    lite_vector *vector = malloc(sizeof vector);
+    vector->length =0;
+    vector->max_capacity=100;
+    vector->data=malloc(sizeof vector->data);
+    //still need to figure out how to return null
+    return vector;
+    
 }
 
 void lv_cleanup(lite_vector* vec){
-}
-
-size_t lv_get_length(lite_vector* vec){
+    free(vec->data);
+    free(vec);
 }
 
 bool lv_clear(lite_vector* vec){
-}
+    vec->length=0;
+    vec->max_capacity=100;
+    vec->data=NULL;
 
+    if(vec->length != 0)
+        return false;
+    return true;
+}
+size_t lv_get_length(lite_vector* vec){
+    return vec->length;
+}
 void* lv_get(lite_vector* vec, size_t index){
+
+    printf("Vector.c address of head %p \n",vec->data);
+
+    if(index == 0)
+        return vec->data;
+
+    void** tmp;
+    tmp=vec->data + 1 * sizeof(vec->data);
+    // for(int i=1;i< index;++i){
+    //     tmp=vec->data + i * sizeof(vec->data);
+    // }
+    return tmp;
+
 }
 
 /**
@@ -29,7 +57,14 @@ void* lv_get(lite_vector* vec, size_t index){
  * must remain unaffected.
  */
 static bool lv_resize(lite_vector* vec){
+    if(vec->length == vec->max_capacity -1){
+        lite_vector *bigger = vec;
+        memcpy(vec,bigger, sizeof(bigger)*10);
+    }
 }
 
 bool lv_append(lite_vector* vec, void* element){
+    vec->data=element;
+    vec->length++;
+
 }
